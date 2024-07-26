7c1b2a93a04cf46795ae82c26ee4ce175f2a6546
BnSplits <bnsplit00@gmail.com>
2024-07-26 17:03:22 +0000

    first commit

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
-sudo pacman -S hplip picom lxappearance ttf-fira-code ttf-font-awesome ttf-nerd-fonts-symbols  zsh broadcom-wl neofetch btop thunar kitty feh polybar rofi evince vim neovim spotify-launcher qt5ct gnome-screenshot fzf ark appimagelauncher
+sudo pacman -S hplip picom lxappearance ttf-fira-code ttf-font-awesome ttf-nerd-fonts-symbols  zsh broadcom-wl neofetch btop thunar kitty feh polybar rofi evince vim neovim spotify-launcher qt5ct gnome-screenshot fzf ark appimagelauncher flatpak
 
 yay -S thorium-browser-bin cava eog clifm visual-studio-code-bin onlyoffice pywal-16-colors diodon arandr
+
+flatpak install flathub com.notesnook.Notesnook
+flatpak install flathub org.onlyoffice.desktopeditors
+
+wget https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst && sudo pacman -U "$PWD/megasync-x86_64.pkg.tar.zst"
