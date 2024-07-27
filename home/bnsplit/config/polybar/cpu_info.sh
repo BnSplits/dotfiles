a385b7b829e73e2d725efab57c33f4aae66e1c55
BnSplits <bnsplit00@gmail.com>
2024-07-27 07:49:58 +0000

    first commit

---
+++ home/bnsplit/config/polybar/cpu_info.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+
+cpu_temp=$(sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}')
+
+echo "${cpu_usage}% (${cpu_temp})"
